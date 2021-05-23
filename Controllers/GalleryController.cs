cd8c7528b7fb732cb7c00719ae648c181886d970
dogancanulgu <78671556+dogancanulgu@users.noreply.github.com>
2021-05-23 03:17:33 +0300

    Galery Added

---
+++ Controllers/GalleryController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcOnlineCommercialAutomation.Models.Classes;
+
+namespace MvcOnlineCommercialAutomation.Controllers
+{
+    public class GalleryController : Controller
+    {
+        // GET: Galery
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var values = c.Products.ToList();
+            return View(values);
+        }
+    }
+}
